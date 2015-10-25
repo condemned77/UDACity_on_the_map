59cb497125d2380f032dfd87d3a813d9bd4fcc11
mjaeger <huNt@h.home>
2015-10-25 18:31:59 +0100

    Restructured storyboard elements of loginViewController.
    - Placeholder text font is now white.

--- UDACity_on_the_map/LoginViewController.swift
+++ UDACity_on_the_map/LoginViewController.swift
@@ -7,3 +7,24 @@
 //
 
 import Foundation
+import UIKit
+
+
+class LoginViewController: UIViewController {
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+
+    override func viewDidLoad() {
+//        self.applyGradientToBackground()
+    }
+    
+    
+    func applyGradientToBackground() {
+        let gradient: CAGradientLayer = CAGradientLayer()
+        gradient.frame = view.bounds
+        gradient.colors = [UIColor.whiteColor().CGColor, UIColor.orangeColor().CGColor]
+        self.view.layer.insertSublayer(gradient, atIndex: 0)
+
+    }
+}
