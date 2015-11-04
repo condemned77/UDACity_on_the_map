c7f229cfd160eee75805fa8573a0e46582f5ee53
mjaeger <huNt@h.home>
2015-11-04 17:45:54 +0100

    Added icons and startup images. renamed ParseMapData to Helpers.
    The helpers file does contain multiple methods that do not belong
    to a particular object, rather they are commonly used within the
    application.

---
+++ UDACity_on_the_map/Helpers.swift
@@ -0,0 +1,25 @@
+//
+//  Helpers.swift
+//  UDACity_on_the_map
+//
+//  Created by jason on 01/11/15.
+//  Copyright © 2015 jason. All rights reserved.
+//
+
+import Foundation
+
+struct Helpers {
+    /* Helper: Given raw JSON, return a usable Foundation object */
+    static func parseJSONWithCompletionHandler(data: NSData, completionHandler: (result: AnyObject!, error: NSError?) -> Void) {
+        
+        var parsedResult: AnyObject!
+        do {
+            parsedResult = try NSJSONSerialization.JSONObjectWithData(data, options: .AllowFragments)
+        } catch {
+            let userInfo = [NSLocalizedDescriptionKey : "Could not parse the data as JSON: '\(data)'"]
+            completionHandler(result: nil, error: NSError(domain: "parseJSONWithCompletionHandler", code: 1, userInfo: userInfo))
+        }
+        
+        completionHandler(result: parsedResult, error: nil)
+    }
+}
--- UDACity_on_the_map/ParseMapData.swift
+++
@@ -1,9 +0,0 @@
-//
-//  ParseMapData.swift
-//  UDACity_on_the_map
-//
-//  Created by jason on 01/11/15.
-//  Copyright © 2015 jason. All rights reserved.
-//
-
-import Foundation
