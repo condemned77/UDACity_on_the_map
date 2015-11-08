2d98990b682445b426ac393ed0860b1e0335906e
mjaeger <huNt@h.home>
2015-11-08 18:09:18 +0100

    ParseAPIClient changes.
    Added method requestStudentLocation, which downloads the student
    location via the ParseAPI.

--- UDACity_on_the_map/ParseAPIClient.swift
+++ UDACity_on_the_map/ParseAPIClient.swift
@@ -7,3 +7,28 @@
 //
 
 import Foundation
+
+class ParseAPIClient: NSObject {
+    /*Convenience method that requests the student location data.
+      The student location data is passed to the completion handler argument, which
+      has to process the received data.
+    */
+    func requestStudentLocation(completionHandler: (result: AnyObject!, error: NSError?) -> Void) {
+        
+        let request = NSMutableURLRequest(URL: NSURL(string: ParseAPIConstants.URLs.STUDENTLOCATION)!)
+        request.addValue(ParseAPIConstants.APPLICATION_ID, forHTTPHeaderField: "X-Parse-Application-Id")
+        request.addValue(ParseAPIConstants.REST_API_Key, forHTTPHeaderField: "X-Parse-REST-API-Key")
+        
+        let session = NSURLSession.sharedSession()
+        
+        let task = session.dataTaskWithRequest(request) { data, response, error in
+            if error != nil { // TODO: Handle error...
+                return
+            }
+            print("data response: \(NSString(data: data!, encoding: NSUTF8StringEncoding))")
+            Helpers.parseJSONWithCompletionHandler(data!, completionHandler: completionHandler)
+        }
+        
+        task.start()
+    }
+}
