1d52df9a503c7d158b913c3c1794901d2f351e43
mjaeger <huNt@h.home>
2015-11-08 17:44:47 +0100

    ListViewController now fills the UITableView.
    Student locations are taken from the UINavigation controller.
    Student's first and last names are populated to the table rows.
    
    Clicking on a table row does open student's URL in safari.

--- UDACity_on_the_map/ListViewController.swift
+++ UDACity_on_the_map/ListViewController.swift
@@ -8,18 +8,47 @@
 
 import UIKit
 
-class ListViewController: UIViewController {
-
+class ListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    @IBOutlet weak var mapPinTableView: UITableView!
+    var studentLocations : [ParseAPIClient.StudentMapData]?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.studentLocations = (self.tabBarController as! MapTabbarController).studentLocations
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
 
-
+        var cell = self.mapPinTableView.dequeueReusableCellWithIdentifier("MapPinCell")
+        
+        if cell == nil {
+            cell = UITableViewCell()
+        }
+        let studentName = "\(self.studentLocations![indexPath.row].firstName) \(self.studentLocations![indexPath.row].lastName)"
+        cell!.textLabel?.text = studentName
+        
+        return cell!
+    }
+    
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.studentLocations!.count
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        let url = self.studentLocations![indexPath.row].mediaURL
+        self.openURLInSafari(url)
+    }
+    
+    
+    func openURLInSafari(url : String) {
+        UIApplication.sharedApplication().openURL(NSURL(string: url)!)
+    }
 }
-
---
+++ UDACity_on_the_map/MapTabbarController.swift
@@ -0,0 +1,15 @@
+//
+//  TabbarController.swift
+//  UDACity_on_the_map
+//
+//  Created by jason on 04/11/15.
+//  Copyright © 2015 jason. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MapTabbarController : UITabBarController {
+    
+    
+}
