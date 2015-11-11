9dd75e0bbcc983fee5ab9793d55fd8d31a488d2f
mjaeger <huNt@h.home>
2015-11-11 20:56:49 +0100

    Added InformationPostingViewController + dismiss
    The MapViewController and ListViewController now present the
    InformationPostingViewController modally, when the pin button
    is pressed.
    
    Storyboard isn't included into commit due to it containing the
    UDACity login data.

---
+++ UDACity_on_the_map/InformationPostingViewController.swift
@@ -0,0 +1,17 @@
+//
+//  InformationPostingViewController.swift
+//  UDACity_on_the_map
+//
+//  Created by jason on 11/11/15.
+//  Copyright © 2015 jason. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class InformationPostingViewController : UIViewController {
+    
+    @IBAction func cancelButtonPressed(sender: UIBarButtonItem) {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+}
